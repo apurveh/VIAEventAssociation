873f2ba89920184cbae3cb601df85cad4ff4c817
apurveh <333324@via.dk>
2025-02-09 23:34:05 +0100

    Added .gitignore file and abstract domain model

--- Assignment1/obj/Debug/net9.0/Assignment1.AssemblyInfo.cs
+++ Assignment1/obj/Debug/net9.0/Assignment1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16a0331c3ddf863c296699118069227a33bd7361")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
