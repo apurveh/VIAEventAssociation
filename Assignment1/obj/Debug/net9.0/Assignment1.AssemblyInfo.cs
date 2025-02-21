9b470c2bf063eed90652871469b0ec881e4e6a19
apurveh <333324@via.dk>
2025-02-21 08:52:11 +0100

    Added concrete domain model

--- Assignment1/obj/Debug/net9.0/Assignment1.AssemblyInfo.cs
+++ Assignment1/obj/Debug/net9.0/Assignment1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16a0331c3ddf863c296699118069227a33bd7361")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7515f20b5f30f57ac345602deb4b952709294c24")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
