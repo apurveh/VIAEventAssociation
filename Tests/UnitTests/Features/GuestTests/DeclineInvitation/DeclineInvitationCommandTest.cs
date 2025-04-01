7be63af9614164db29cafec37c2b901066eaa9d9
Cristian Josan <josan.c2003@gmail.com>
2025-04-01 18:59:11 +0300

    Add commands, handlers and tests for UC14 and UC15.

---
+++ Tests/UnitTests/Features/GuestTests/DeclineInvitation/DeclineInvitationCommandTest.cs
@@ -0,0 +1,41 @@
+﻿using VIAEventAssociation.Core.Application.CommandDispatching.Commands.Guest;
+using VIAEventAssociation.Core.Tools.OperationResult;
+
+namespace UnitTests.Features.GuestTests.DeclineInvitation;
+
+public class DeclineInvitationCommandTest
+{
+    // UC15.S1
+    [Fact]
+    public void Create_ValidInput_Success()
+    {
+        // Arrange
+        var eventId = "EID" + Guid.NewGuid();
+        var guestId = "GID" + Guid.NewGuid();
+        
+        // Act
+        var result = DeclineInvitationCommand.Create(eventId, guestId);
+        var command = result.Payload;
+        
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.Equal(guestId, command.Id.Value);
+        Assert.Equal(eventId, command.EventId.Value);
+    }
+    
+    // UC15.F1
+    [Fact]
+    public void Create_WithInvalidEventId_ShouldReturnError()
+    {
+        // Arrange
+        var eventId = "EDD" + Guid.NewGuid();
+        var guestId = "GID" + Guid.NewGuid();
+        
+        // Act
+        var result = DeclineInvitationCommand.Create(eventId, guestId);
+        
+        // Assert
+        Assert.True(result.IsFailure);
+        Assert.Contains(Error.InvalidPrefix, result.Error.GetAllErrors());
+    }
+}
---
+++ src/Core/VIAEventAssociation.Core.Application/CommandDispatching/Commands/Guest/AcceptInvitationCommand.cs
@@ -0,0 +1,16 @@
+﻿using VIAEventAssociation.Core.Domain.Aggregates.Events;
+using VIAEventAssociation.Core.Domain.Aggregates.Guests;
+using VIAEventAssociation.Core.Tools.OperationResult;
+
+namespace VIAEventAssociation.Core.Application.CommandDispatching.Commands.Guest;
+
+public class AcceptInvitationCommand : GuestCommand
+{
+    private AcceptInvitationCommand(EventId eventId, GuestId guestId) : base(eventId, guestId) { }
+
+    public static Result<AcceptInvitationCommand> Create(string eventIdAsString, string guestIdAsString)
+    {
+        return Create(eventIdAsString, guestIdAsString, (eventId, guestId) =>
+            new AcceptInvitationCommand(eventId, guestId));
+    }
+}
