64a2a5e7520d46e597dc9341515313a348f937ab
Cristian Josan <josan.c2003@gmail.com>
2025-05-04 23:51:56 +0200

    Add other files.

---
+++ src/Infrastructure/ViaEventAssociation.Infrastructure.EfDmPersistence/Migrations/20250504171445_InitialCreate.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ViaEventAssociation.Infrastructure.SqliteDmPersistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    EventStart = table.Column<DateTime>(type: "datetime", nullable: true),
+                    EventEnd = table.Column<DateTime>(type: "datetime", nullable: true),
+                    Visibility = table.Column<string>(type: "TEXT", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    NumberOfGuests = table.Column<int>(type: "INTEGER", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Guests",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Guests", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Organizers",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organizers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Participation",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    GuestId = table.Column<string>(type: "TEXT", nullable: false),
+                    EventId = table.Column<string>(type: "TEXT", nullable: false),
+                    ParticipationType = table.Column<int>(type: "INTEGER", nullable: false),
+                    ParticipationStatus = table.Column<int>(type: "INTEGER", nullable: false),
+                    Discriminator = table.Column<string>(type: "TEXT", maxLength: 13, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Participation", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Participation_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Participation_Guests_GuestId",
+                        column: x => x.GuestId,
+                        principalTable: "Guests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Participation_EventId",
+                table: "Participation",
+                column: "EventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Participation_GuestId",
+                table: "Participation",
+                column: "GuestId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropTable(
+                name: "Organizers");
+
+            migrationBuilder.DropTable(
+                name: "Participation");
+
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            migrationBuilder.DropTable(
+                name: "Guests");
+        }
+    }
+}
---
+++ src/Presentation/ViaEventAssociation.Presentation.WebAPI/Program.cs
@@ -0,0 +1,61 @@
+using Microsoft.EntityFrameworkCore;
+using ViaEventAssociation.Core.Application.CommandDispatching.Dispatcher;
+using ViaEventAssociation.Core.Application.Extensions;
+using ViaEventAssociation.Core.Application.Features.Dispatcher;
+using ViaEventAssociation.Core.Domain;
+using ViaEventAssociation.Core.Domain.Agregates.Events;
+using ViaEventAssociation.Core.Domain.Agregates.Guests;
+using ViaEventAssociation.Core.QueryContracts.Contracts;
+using ViaEventAssociation.Core.QueryContracts.Queries;
+using ViaEventAssociation.Core.QueryContracts.QueryDispatching;
+using ViaEventAssociation.Core.Tools.ObjectMapper;
+using ViaEventAssociation.Infrastructure.EfcQueries;
+using ViaEventAssociation.Infrastructure.EfcQueries.Queries;
+using ViaEventAssociation.Infrastructure.SqliteDmPersistence.EventPersistence;
+using ViaEventAssociation.Infrastructure.SqliteDmPersistence.GuestPersistence;
+using ViaEventAssociation.Infrastructure.SqliteDmPersistence.UnitOfWork;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DmContext>(options =>
+    options.UseSqlite(
+        @"Data Source = C:\VIA University\Semester 6\DCA1\ViaEventAssociation\src\Infrastructure\ViaEventAssociation.Infrastructure.EfDmPersistence\DBProduction.db"));
+
+builder.Services.AddScoped<IUnitOfWork, SqliteUnitOfWork>();
+
+builder.Services.AddScoped<IEventRepository, EventEfRepository>();
+builder.Services.AddScoped<IGuestRepository, GuestEfRepository>();
+builder.Services.AddScoped<ICommandDispatcher, CommandDispatcher>();
+
+builder.Services.AddScoped<DbproductionContext>(provider => DbproductionContext.SetupContext());
+builder.Services.AddScoped<IMapper, ConcreteObjectMapper>();
+builder.Services.AddScoped<IQueryDispatcher, QueryDispatcher>();
+
+builder.Services.RegisterCommandHandlers();
+builder.Services.RegisterQueryHandlers();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
